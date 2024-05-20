901612a62f896b13ce07c30a9719fcabee52a8dd
mdavid-mufin <m.david@mufin.co.kr>
2024-05-20 19:05:27 +0300

    Pach-Fix-Build-Errors

---
+++ dist/domain/entity.js
@@ -0,0 +1,18 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+var _v = _interopRequireDefault(require("uuid"));
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+class Entity {
+  /**
+   * Representa uma entidade
+   * @param {string} id Identificador da Entidade
+   */
+  constructor(id) {
+    this.id = !id ? (0, _v.default)() : id;
+  }
+}
+var _default = exports.default = Entity;
---
+++ dist/repositories/index.js
@@ -0,0 +1,13 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+Object.defineProperty(exports, "personRepository", {
+  enumerable: true,
+  get: function () {
+    return _person.default;
+  }
+});
+var _person = _interopRequireDefault(require("./person.repository"));
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
